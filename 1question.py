aba69324c94fb12b76329a903d07bb8e2e45a722
subhi006 <114250085+subhi006@users.noreply.github.com>
2023-10-29 13:52:57 +0530

    assignment 4

---
+++ 1question.py
@@ -0,0 +1,2 @@
+#Write a python script to take your name as input from the user and then print it.
+print("Your name is:",str(input("Enter your name:")))
---
+++ 3question.py
@@ -0,0 +1,6 @@
+"""Write a python script which takes two numbers from the user, then calculate their sum
+and display the result."""
+
+x=int(input("Enter a first no."))
+y=int(input("Enter a second no."))
+print("Sum is :",x+y)
